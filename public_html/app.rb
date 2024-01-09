e0228587922d3255ec1304a50c7cfb9d4645b72f
bonneville <jeanlucbonneville@gmail.com>
2024-01-09 22:08:12 +0100

    5 22222222664562

--- public_html/app.rb
+++ public_html/app.rb
@@ -2,31 +2,66 @@
 require 'sinatra/base'
 require 'dotenv/load'
 
-
 # Include the correct file name: contact_form.rb
 require_relative 'contact_form'
 
 class MyApp < Sinatra::Base
-  # Configure Sinatra to run on port 5678 (ou tout autre port de votre choix)
-  set :port, 5678
+  # Configure Sinatra to run on port 4567 (or any other port of your choice)
+  set :port, 4567
 
-  # Configurer le répertoire des vues
+  # Configure the views directory
   configure do
     set :public_folder, File.join(File.dirname(__FILE__), 'public')
+    set :views, File.dirname(__FILE__) + '/views'
   end
 
-  # Route principale
+  # Main route
   get '/' do
     erb :index
   end
 
-  # Autres routes et configurations peuvent être ajoutées ici
+  # Other routes and configurations can be added here
   get '/contact' do
     erb :contact
   end
 
-  # Montrez que le formulaire de contact est une extension de l'application principale
+  # Show that the contact form is an extension of the main application
   register ContactForm
+
+  post '/submit' do
+    begin
+      # Your existing code here
+      array = {
+        # ... (existing array initialization)
+      }
+
+      email_to = "jeanlucbonneville@gmail.com"
+
+      # Validate and process the form data
+      params.each { |key, value| array[key] = verify_input(value) }
+
+      array["isSuccess"] = true
+      email_text = ""
+
+      %w[firstname name email phone message].each do |field|
+        if array[field].empty?
+          array["#{field}Error"] = "Le champ #{field.capitalize} est requis."
+          array["isSuccess"] = false
+        else
+          email_text += "#{field.capitalize}: #{array[field]}\n"
+        end
+      end
+
+      send_email_with_mail(email_text, email_to) if array["isSuccess"]
+      send_email_with_sendgrid(email_text, email_to) if array["isSuccess"]
+
+      content_type :json
+      array.to_json
+    rescue => e
+      puts "Erreur rencontrée: #{e.message}"
+      # Handle the error as needed, you can return a custom error message or page
+    end
+  end
 
   run! if app_file == $0
 end
--- public_html/contact_form.rb
+++ public_html/contact_form.rb
@@ -1,72 +1,78 @@
+module ContactForm
 # contact.rb
-require 'sinatra'
-require 'json'
-require 'sendgrid-ruby'
-require 'dotenv/load'
+  require 'sinatra'
+  require 'json'
+  require 'sendgrid-ruby'
+  require 'dotenv/load'
 
-Dotenv.load
+  Dotenv.load
 
-def is_email(var)
-  (var =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i) != nil
-end
-
-def is_phone(var)
-  (var =~ /^\d+$/) != nil
-end
-
-def verify_input(var)
-  var.strip.gsub(/<|>/, '')
-end
-
-def send_email_with_mail(email_text, email_to)
-  `echo "#{email_text}" | mail -s "Message de jlbcodeur !!" #{email_to} --`
-end
-
-def send_email_with_sendgrid(email_text, email_to)
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  from = SendGrid::Email.new(email: "#{params['firstname']} #{params['name']} <#{params['email']}>")
-  to = SendGrid::Email.new(email: email_to)
-  subject = 'Sujet de l\'e-mail'
-  content = SendGrid::Content.new(type: 'text/plain', value: email_text)
-  mail = SendGrid::Mail.new(from, subject, to, content)
-
-  sg.client.mail._('send').post(request_body: mail.to_json)
-end
-
-post '/submit' do
-  array = {
-    "firstname" => " ",
-    "name" => " ",
-    "email" => " ",
-    "phone" => " ",
-    "message" => " ",
-    "firstnameError" => " ",
-    "nameError" => " ",
-    "emailError" => " ",
-    "phoneError" => " ",
-    "messageError" => " ",
-    "isSuccess" => false
-  }
-
-  email_to = "jeanlucbonneville@gmail.com"
-
-  params.each { |key, value| array[key] = verify_input(value) }
-
-  array["isSuccess"] = true
-  email_text = ""
-
-  %w[firstame name email phone message].each do |field|
-    if array[field].empty?
-      array["#{field}Error"] = "Le champ #{field.capitalize} est requis."
-      array["isSuccess"] = false
-    else
-      email_text += "#{field.capitalize}: #{array[field]}\n"
-    end
+  def is_email(var)
+    (var =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i) != nil
   end
 
-  send_email_with_mail(email_text, email_to) if array["isSuccess"]
-  send_email_with_sendgrid(email_text, email_to) if array["isSuccess"]
+  def is_phone(var)
+    (var =~ /^\d+$/) != nil
+  end
 
-  content_type :json
-  array.to_json
+  def verify_input(var)
+    var.strip.gsub(/<|>/, '')
+  end
+
+  def send_email_with_mail(email_text, email_to)
+    `echo "#{email_text}" | mail -s "Message de jlbcodeur !!" #{email_to} --`
+  end
+
+  def send_email_with_sendgrid(email_text, email_to)
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    from = SendGrid::Email.new(email: "#{params['firstname']} #{params['name']} <#{params['email']}>")
+    to = SendGrid::Email.new(email: email_to)
+    subject = 'Sujet de l\'e-mail'
+    content = SendGrid::Content.new(type: 'text/plain', value: email_text)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+
+  def self.post_submit
+    Sinatra::Base.post '/submit' do
+      # le contenu de votre route POST '/submit' existante
+      
+      array = {
+        "firstname" => " ",
+        "name" => " ",
+        "email" => " ",
+        "phone" => " ",
+        "message" => " ",
+        "firstnameError" => " ",
+        "nameError" => " ",
+        "emailError" => " ",
+        "phoneError" => " ",
+        "messageError" => " ",
+        "isSuccess" => false
+      }
+      
+      email_to = "jeanlucbonneville@gmail.com"
+      
+      params.each { |key, value| array[key] = verify_input(value) }
+      
+      array["isSuccess"] = true
+      email_text = ""
+      
+      %w[firstname name email phone message].each do |field|
+        if array[field].empty?
+          array["#{field}Error"] = "Le champ #{field.capitalize} est requis."
+          array["isSuccess"] = false
+        else
+          email_text += "#{field.capitalize}: #{array[field]}\n"
+        end
+      end
+      
+      send_email_with_mail(email_text, email_to) if array["isSuccess"]
+      send_email_with_sendgrid(email_text, email_to) if array["isSuccess"]
+      
+      content_type :json
+      array.to_json
+    end
+  end
 end
